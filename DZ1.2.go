18cc2193378435744082c4bb56e2fdcc53aa243e
Sht-ua <54975698+Sht-ua@users.noreply.github.com>
2020-05-04 16:45:02 +0300

    Update DZ1.2.go

--- DZ1.2.go
+++ DZ1.2.go
@@ -4,39 +4,42 @@ import "fmt"
 
 type human struct {
 	Name string
+
 }
 type worker struct {
 	human
 	Company string
 }
-type Doctor struct {
-	worker
-	Stack []string
+type  Doctor struct {
+	 worker
+     Stack[]string
 }
-type Driver struct {
+type  Driver struct {
 	worker
-	DrivingExperience []string
+	DrivingExperience[] string
 }
-type Policeman struct {
+type  Policeman struct {
 	worker
 	Rank string
 }
-type Teacher struct {
+type  Teacher struct {
 	worker
 	Degree string
 }
-type Firefighter struct {
+type  Firefighter struct {
 	worker
 	Rank2 string
 }
 
-func main() {
-	user := Firefighter{Rank2: "Дежурный"}
-	user1 := worker{Company: "Рога и копыта"}
-	user3 := human{Name: "Ираклий"}
-	fmt.Println(user)
-	fmt.Println(user1)
-	fmt.Println(user3)
+func main(){
+user := Firefighter{Rank2: "Дежурный"}
+user1 := worker{Company: "Рога и копыта"}
+user3 := human{Name: "Ираклий"}
+
+	fmt.Println(user.Rank2)
+	fmt.Println(user1.Company)
+	fmt.Println(user3.Name)
+
 }
 
 
