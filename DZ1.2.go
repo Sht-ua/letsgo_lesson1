2791ed6ade0f757355406ebd2787f05531633865
Sht-ua <54975698+Sht-ua@users.noreply.github.com>
2020-05-04 17:21:29 +0300

    Update DZ1.2.go

--- DZ1.2.go
+++ DZ1.2.go
@@ -4,19 +4,18 @@ import "fmt"
 
 type human struct {
 	Name string
-
 }
 type worker struct {
 	human
 	Company string
 }
-type  Doctor struct {
-	 worker
-     Stack[]string
+type  doctor struct {
+	worker
+	Stack[]string
 }
 type  Driver struct {
 	worker
-	DrivingExperience[] string
+	DrivingExperience string
 }
 type  Policeman struct {
 	worker
@@ -32,15 +31,22 @@ type  Firefighter struct {
 }
 
 func main(){
-user := Firefighter{Rank2: "Дежурный"}
-user1 := worker{Company: "Рога и копыта"}
-user3 := human{Name: "Ираклий"}
-
+	user := Firefighter{Rank2: "Дежурный"}
+	user1 := worker{Company: "Рога и копыта"}
+	user3 := human{Name: "Ираклий"}
+	x:= Driver{DrivingExperience: "Опытный"}
+	v := human{"Иван"}
+	p:=&v
+	p.Name = "Анатолий"
+	//user11 := &v
+	
+	
+	fmt.Println(v.Name)
+	fmt.Println(user.Name)
 	fmt.Println(user.Rank2)
 	fmt.Println(user1.Company)
 	fmt.Println(user3.Name)
+	fmt.Println(x.DrivingExperience)
+
 
 }
-
-
-
